2e7bf1f8513219b4576fbf6eb6224dfa7ceaf8d6
openthings <openthings@163.com>
2018-07-18 13:48:03 +0800

    add custom build tool for tf1.8c.

---
+++ deploy/w3-kubeflow-worker-pullfromaliyun1.8c.sh
@@ -0,0 +1,27 @@
+## 添加Tag for registry.cn-hangzhou.aliyuncs.com/openthings
+MY_REGISTRY=registry.cn-hangzhou.aliyuncs.com/openthings
+MY_IMAGE_CPU=tensorflow-1.8.0-notebook-cpu:1.8.0
+#MY_IMAGE_GPU=tensorflow-1.8.0-notebook-gpu:v20180710-bf1c2f0a
+
+## Push镜像
+## Tag to original docker iamges name.
+
+echo ""
+echo "1. tensorflow-1.8.0-notebook-cpu"
+echo "PULL: ${MY_REGISTRY}/kubeflow-images-public-${MY_IMAGE_CPU}"
+docker pull ${MY_REGISTRY}/kubeflow-images-public-${MY_IMAGE_CPU}
+docker tag ${MY_REGISTRY}/kubeflow-images-public-${MY_IMAGE_CPU} openthings/${MY_IMAGE_CPU} 
+
+#echo ""
+#echo "2. tensorflow-1.8.0-notebook-gpu"
+#echo "PULL: ${MY_REGISTRY}/kubeflow-images-public-${MY_IMAGE_GPU}"
+#docker pull ${MY_REGISTRY}/kubeflow-images-public-${MY_IMAGE_GPU}
+#docker tag ${MY_REGISTRY}/kubeflow-images-public-${MY_IMAGE_GPU} gcr.io/kubeflow-images-public/${MY_IMAGE_GPU}
+
+echo ""
+echo "FINISHED."
+echo ""
+
+
+
+
